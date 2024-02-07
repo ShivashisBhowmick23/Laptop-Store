bd92952407a0afbfc8a7da853219d8c7e09f629f
ShivashisBhowmick23 <shiashis.bhowmick@outlook.com>
2024-02-07 19:31:10 +0530

    Swagger Implementation of Endpoints

--- src/main/java/com/boot/laptop/controller/LaptopController.java
+++ src/main/java/com/boot/laptop/controller/LaptopController.java
@@ -6,6 +6,9 @@ import com.boot.laptop.model.Laptop;
 import com.boot.laptop.request.LaptopRequest;
 import com.boot.laptop.response.LaptopResponse;
 import com.boot.laptop.service.LaptopService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,6 +29,14 @@ public class LaptopController {
     }
 
     @PostMapping(URLConstant.ADD_LAPTOP_INTO_STORE)
+    @Operation
+            (summary = "Add Laptop into the database",
+            description = " addLaptop method will add into the database and return inserted Laptop",
+            method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "404", description = "Resource not found"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")})
     public ResponseEntity<LaptopResponse> addLaptop(@RequestBody LaptopRequest laptopRequest) {
         Laptop request = laptopMapper.mapLaptopRequestToLaptop(laptopRequest);
         laptopService.addLaptop(request);
@@ -36,6 +47,14 @@ public class LaptopController {
     }
 
     @GetMapping(URLConstant.LAPTOP_COLLECTION)
+    @Operation
+            (summary = "Get all the laptops from the database",
+                    description = "getAllLaptop method will return all the laptops as list",
+                    method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "404", description = "Resource not found"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")})
     public ResponseEntity<List<LaptopResponse>> getAllLaptop() {
         List<Laptop> laptopList = laptopService.getAllLaptop();
         List<LaptopResponse> responseList = laptopMapper.mapLaptopListToLaptopResponseList(laptopList);
