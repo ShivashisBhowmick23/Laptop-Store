88c9bf0202b9f8fc4de7d41b558c5f37a1d1c2f1
ShivashisBhowmick23 <shiashis.bhowmick@outlook.com>
2024-02-07 22:31:54 +0530

    Swagger Implementation of Endpoints

--- src/main/java/com/boot/laptop/controller/LaptopController.java
+++ src/main/java/com/boot/laptop/controller/LaptopController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(URLConstant.LAPTOP_STORE)
@@ -29,14 +30,8 @@ public class LaptopController {
     }
 
     @PostMapping(URLConstant.ADD_LAPTOP_INTO_STORE)
-    @Operation
-            (summary = "Add Laptop into the database",
-            description = " addLaptop method will add into the database and return inserted Laptop",
-            method = "POST")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successful operation"),
-            @ApiResponse(responseCode = "404", description = "Resource not found"),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error")})
+    @Operation(summary = "Add Laptop into the database", description = " addLaptop method will add into the database and return inserted Laptop", method = "POST")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Successful operation"), @ApiResponse(responseCode = "404", description = "Resource not found"), @ApiResponse(responseCode = "500", description = "Internal Server Error")})
     public ResponseEntity<LaptopResponse> addLaptop(@RequestBody LaptopRequest laptopRequest) {
         Laptop request = laptopMapper.mapLaptopRequestToLaptop(laptopRequest);
         laptopService.addLaptop(request);
@@ -47,18 +42,21 @@ public class LaptopController {
     }
 
     @GetMapping(URLConstant.LAPTOP_COLLECTION)
-    @Operation
-            (summary = "Get all the laptops from the database",
-                    description = "getAllLaptop method will return all the laptops as list",
-                    method = "GET")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successful operation"),
-            @ApiResponse(responseCode = "404", description = "Resource not found"),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error")})
+    @Operation(summary = "Get all the laptops from the database", description = "getAllLaptop method will return all the laptops as list", method = "GET")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Successful operation"), @ApiResponse(responseCode = "404", description = "Resource not found"), @ApiResponse(responseCode = "500", description = "Internal Server Error")})
     public ResponseEntity<List<LaptopResponse>> getAllLaptop() {
         List<Laptop> laptopList = laptopService.getAllLaptop();
         List<LaptopResponse> responseList = laptopMapper.mapLaptopListToLaptopResponseList(laptopList);
         return ResponseEntity.ok(responseList);
+    }
+
+    @GetMapping(URLConstant.LAPTOP_BY_LAPTOP_ID)
+    @Operation(summary = "Fetch Laptop By LaptopId", description = "getAllLaptop method will return all the laptops as list", method = "GET")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Successful operation"), @ApiResponse(responseCode = "404", description = "Resource not found"), @ApiResponse(responseCode = "500", description = "Internal Server Error")})
+    public ResponseEntity<LaptopResponse> getLaptopById(@PathVariable("laptop_id") int laptop_id) {
+        laptopService.getLaptopById(laptop_id);
+        LaptopResponse response = laptopMapper.mapLaptopToLaptopResponseByLaptopID(laptop_id);
+        return ResponseEntity.ok(response);
     }
 
 }
--- src/main/java/com/boot/laptop/mapper/LaptopMapper.java
+++ src/main/java/com/boot/laptop/mapper/LaptopMapper.java
@@ -1,15 +1,21 @@
 package com.boot.laptop.mapper;
 
 import com.boot.laptop.model.Laptop;
+import com.boot.laptop.repository.LaptopRepository;
 import com.boot.laptop.request.LaptopRequest;
 import com.boot.laptop.response.LaptopResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class LaptopMapper {
+
+    @Autowired
+    LaptopRepository laptopRepository;
 
     public LaptopRequest mapLaptopToLaptopRequest(Laptop laptop) {
         LaptopRequest laptopRequest = new LaptopRequest();
@@ -26,6 +32,27 @@ public class LaptopMapper {
         laptopResponse.setLaptop_name(laptop.getLaptop_name());
 
         return laptopResponse;
+    }
+
+    public LaptopResponse mapLaptopToLaptopResponseByLaptopID(int laptop_id) {
+        // Retrieve the Laptop object by its ID
+        Optional<Laptop> optionalLaptop = laptopRepository.findById(laptop_id);
+
+        if (optionalLaptop.isPresent()) {
+            Laptop laptop = optionalLaptop.get();
+
+            // Map properties from Laptop to LaptopResponse
+            LaptopResponse laptopResponse = new LaptopResponse();
+            laptopResponse.setLaptop_id(laptop.getLaptop_id());
+            laptopResponse.setLaptop_price(laptop.getLaptop_price());
+            laptopResponse.setLaptop_name(laptop.getLaptop_name());
+            // Map other properties as needed
+
+            return laptopResponse;
+        } else {
+            // Handle case where Laptop object with the specified ID is not found
+            return null;
+        }
     }
 
     public Laptop mapLaptopRequestToLaptop(LaptopRequest laptopRequest) {
@@ -58,28 +85,6 @@ public class LaptopMapper {
 
         return laptopResponseList;
 
-        /* Explanation of mapLaptopListToLaptopResponseList method
-        List<LaptopResponse> laptopResponseList = new ArrayList<>();:
-
-This line creates a new ArrayList named laptopResponseList that will hold instances of the LaptopResponse class.
-for (Laptop laptop : laptopList) {:
-
-This starts a loop that iterates over each Laptop object in the laptopList parameter passed to the method.
-LaptopResponse laptopResponse = new LaptopResponse();:
-
-Inside the loop, a new instance of LaptopResponse is created for each Laptop object in the laptopList.
-Mapping:
-
-Each field of the laptopResponse object is set based on the corresponding field of the laptop object. For example:
-laptopResponse.setId(laptop.getId()); sets the id field of the laptopResponse object to the id field of the laptop object.
-laptopResponse.setBrand(laptop.getBrand()); sets the brand field of the laptopResponse object to the brand field of the laptop object.
-Similarly, other fields like model, processor, ramSize, and storageSize are mapped from the laptop object to the laptopResponse object.
-laptopResponseList.add(laptopResponse);:
-
-After all fields are mapped, the laptopResponse object is added to the laptopResponseList.
-return laptopResponseList;:
-
-After all Laptop objects in the laptopList have been processed, the method returns the laptopResponseList, which contains LaptopResponse objects mapped from the laptopList.*/
     }
 }
 
