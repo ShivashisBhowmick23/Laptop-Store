dc2826a9b4a7cfabe98fc216d70ff95166a31d85
ShivashisBhowmick23 <shiashis.bhowmick@outlook.com>
2024-02-07 22:51:18 +0530

    Little customization

--- src/main/java/com/boot/laptop/swagger/SwaggerConfig.java
+++ src/main/java/com/boot/laptop/swagger/SwaggerConfig.java
@@ -11,6 +11,6 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI().info(new Info().title("My Laptop Store").description("Laptop Controller").version("1.0").license(new License().name("Apache 2.0").url("http://springdoc.org")));
+        return new OpenAPI().info(new Info().title("My Laptop Store").description(" Methods of Laptop Controller").version("1.0").license(new License().name("ShivashisBhowmick").url("https://github.com/ShivashisBhowmick23/Laptop-Store/tree/develop")));
     }
 }
